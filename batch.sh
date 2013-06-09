efc7bcdf16f38cde8406fef2f58deb08c1db6022
Alexander Stein <al@ila5.info>
2013-06-09 12:29:24 +0300

    Move batch.sh commands to auto_patcher and call automatically if files not detected; allow legacy call from batch.sh via sourcing auto_patcher.

--- batch.sh
+++ batch.sh
@@ -6,11 +6,7 @@ if [ -e "$AUTOPATCHER" ]; then
     source $AUTOPATCHER
 else
     echo "Cannot complete batch operations, cannot find file "$AUTOPATCHER"."
+    exit 1
 fi
 
-cd "$ROOT/patch_tools"
-tar cvzf ../patch_tools.tgz *.jar updatecert.pem updatekey.pk8 boot cygwin mac linux
-
-cd "$ROOT"
-tar cvzf patch_patches.tgz patches
-zip autopatcher.zip auto_patcher patch_tools.tgz patch_patches.tgz
+prep_patches
